9cd7aaf79548119071093819e8ecd936d1305ecb
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-03-06 20:51:44 -0500

    make tests more descriptive

--- tests/test_one_cow.c
+++ tests/test_one_cow.c
@@ -1,32 +1,43 @@
 #include "../tls.h"
 #include <assert.h>
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 #define TLS_SIZE 5
 
 pthread_t thread_create, thread_cow;
 char write_buffer[TLS_SIZE], read_buffer[TLS_SIZE];
 
-static void *create() {
-  const char *str = "hello";
-  assert(tls_create(TLS_SIZE) == 0);
+static void *cow(void *arg) {
+  printf("thread: %lu cloning TLS from thread: %lu\n", (size_t)pthread_self(),
+         (size_t)thread_create);
+  assert(tls_clone(thread_create) == 0);
+
+  const char *str = "world";
   strcpy(write_buffer, str);
+  printf("thread: %lu triggering COW\n", (size_t)pthread_self());
   assert(tls_write(0, TLS_SIZE, write_buffer) == 0);
 
-  sleep(1); // allow thread_cow to clone and write
-
+  printf("thread: %lu reading from TLS after COW\n", (size_t)pthread_self());
   assert(tls_read(0, TLS_SIZE, read_buffer) == 0);
   assert(strcmp(read_buffer, str) == 0);
   assert(tls_destroy() == 0);
   return 0;
 }
 
-static void *cow(void *arg) {
-  const char *str = "world";
-  assert(tls_clone(thread_create) == 0);
+static void *create() {
+  assert(tls_create(TLS_SIZE) == 0);
+  printf("thread: %lu created TLS\n", (size_t)thread_create);
+
+  const char *str = "hello";
   strcpy(write_buffer, str);
-  assert(tls_write(0, TLS_SIZE, write_buffer) == 0); // COW
+  printf("thread: %lu writing to TLS\n", (size_t)pthread_self());
+  assert(tls_write(0, TLS_SIZE, write_buffer) == 0);
+
+  pthread_create(&thread_cow, 0, &cow, 0);
+  pthread_join(thread_cow, NULL);
+
+  printf("thread: %lu reading from TLS\n", (size_t)pthread_self());
   assert(tls_read(0, TLS_SIZE, read_buffer) == 0);
   assert(strcmp(read_buffer, str) == 0);
   assert(tls_destroy() == 0);
@@ -34,8 +45,7 @@ static void *cow(void *arg) {
 }
 
 int main() {
+  printf("main thread: %lu\n", (size_t)pthread_self());
   pthread_create(&thread_create, 0, &create, 0);
-  pthread_create(&thread_cow, 0, &cow, 0);
-  pthread_join(thread_cow, NULL);
   pthread_join(thread_create, NULL);
 }
