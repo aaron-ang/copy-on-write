3b43fd4df5dc357af24de8935eeb317bff7c426c
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-03-06 22:56:02 -0500

    create test_many_cow

--- tests/test_one_cow.c
+++ tests/test_one_cow.c
@@ -8,17 +8,18 @@
 pthread_t thread_create, thread_cow;
 char write_buffer[TLS_SIZE], read_buffer[TLS_SIZE];
 
-static void *cow(void *arg) {
-  printf("thread: %lu cloning TLS from thread: %lu\n", (size_t)pthread_self(),
+static void *cow() {
+  size_t tid = (size_t)pthread_self();
+  printf("thread: %lu cloning TLS from thread: %lu\n", tid,
          (size_t)thread_create);
   assert(tls_clone(thread_create) == 0);
 
   const char *str = "world";
   strcpy(write_buffer, str);
-  printf("thread: %lu triggering COW\n", (size_t)pthread_self());
+  printf("thread: %lu triggering COW\n", tid);
   assert(tls_write(0, TLS_SIZE, write_buffer) == 0);
 
-  printf("thread: %lu reading from TLS after COW\n", (size_t)pthread_self());
+  printf("thread: %lu reading from TLS after COW\n", tid);
   assert(tls_read(0, TLS_SIZE, read_buffer) == 0);
   assert(strcmp(read_buffer, str) == 0);
   assert(tls_destroy() == 0);
@@ -26,18 +27,19 @@ static void *cow(void *arg) {
 }
 
 static void *create() {
+  size_t tid = (size_t)pthread_self();
   assert(tls_create(TLS_SIZE) == 0);
-  printf("thread: %lu created TLS\n", (size_t)thread_create);
+  printf("thread: %lu created TLS\n", tid);
 
   const char *str = "hello";
   strcpy(write_buffer, str);
-  printf("thread: %lu writing to TLS\n", (size_t)pthread_self());
+  printf("thread: %lu writing to TLS\n", tid);
   assert(tls_write(0, TLS_SIZE, write_buffer) == 0);
 
   pthread_create(&thread_cow, 0, &cow, 0);
   pthread_join(thread_cow, NULL);
 
-  printf("thread: %lu reading from TLS\n", (size_t)pthread_self());
+  printf("thread: %lu reading from TLS\n", tid);
   assert(tls_read(0, TLS_SIZE, read_buffer) == 0);
   assert(strcmp(read_buffer, str) == 0);
   assert(tls_destroy() == 0);
